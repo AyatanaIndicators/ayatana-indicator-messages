cbe25167c3e832b3bb67126648e9afd0dc26fa1c
Ted Gould <ted@canonical.com>
2009-01-21 19:33:58 -0600

    Adding in a show that I forgot :(

--- src/im-menu-item.c
+++ src/im-menu-item.c
@@ -96,6 +96,7 @@ im_menu_item_init (ImMenuItem *self)
 	gtk_box_pack_start(GTK_BOX(priv->hbox), GTK_WIDGET(priv->time), FALSE, TRUE, 3);
 	gtk_box_pack_start(GTK_BOX(priv->hbox), GTK_WIDGET(priv->user), TRUE,  TRUE, 3);
 	gtk_box_pack_start(GTK_BOX(priv->hbox), GTK_WIDGET(priv->icon), FALSE, TRUE, 3);
+	gtk_widget_show(GTK_WIDGET(priv->hbox));
 
 	gtk_container_add(GTK_CONTAINER(self), priv->hbox);
 
