d585baaa4ae70e6603e6bacc7d2e4d8acbb914fe
Ted Gould <ted@canonical.com>
2009-05-27 12:39:18 +0200

    Starting the service if it isn't there

--- src/indicator-messages.c
+++ src/indicator-messages.c
@@ -23,6 +23,8 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <string.h>
 #include <gtk/gtk.h>
 #include <libdbusmenu-gtk/menu.h>
+#include <dbus/dbus-glib.h>
+#include <dbus/dbus-glib-bindings.h>
 
 #include <libindicator/indicator.h>
 INDICATOR_SET_VERSION
@@ -57,6 +59,14 @@ get_icon (void)
 GtkMenu *
 get_menu (void)
 {
+	guint returnval = 0;
+	GError * error = NULL;
+
+	DBusGConnection * connection = dbus_g_bus_get(DBUS_BUS_SESSION, NULL);
+	DBusGProxy * proxy = dbus_g_proxy_new_for_name(connection, DBUS_SERVICE_DBUS, DBUS_PATH_DBUS, DBUS_INTERFACE_DBUS);
+
+	org_freedesktop_DBus_start_service_by_name (proxy, INDICATOR_MESSAGES_DBUS_NAME, 0, &returnval, &error);
+
 	return GTK_MENU(dbusmenu_gtkmenu_new(INDICATOR_MESSAGES_DBUS_NAME, INDICATOR_MESSAGES_DBUS_OBJECT));
 }
 
