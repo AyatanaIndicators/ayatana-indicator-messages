e7695d9f9da90e12aaefe490e46e18d3b32f33dd
Ted Gould <ted@canonical.com>
2009-08-25 22:08:41 -0500

    Building an object to represent the service over DBus.

---
+++ src/messages-service-dbus.c
@@ -0,0 +1,84 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "messages-service-dbus.h"
+
+typedef struct _MessageServiceDbusPrivate MessageServiceDbusPrivate;
+
+struct _MessageServiceDbusPrivate
+{
+	guint temp;
+};
+
+#define MESSAGE_SERVICE_DBUS_GET_PRIVATE(o) \
+(G_TYPE_INSTANCE_GET_PRIVATE ((o), MESSAGE_SERVICE_DBUS_TYPE, MessageServiceDbusPrivate))
+
+static void message_service_dbus_class_init (MessageServiceDbusClass *klass);
+static void message_service_dbus_init       (MessageServiceDbus *self);
+static void message_service_dbus_dispose    (GObject *object);
+static void message_service_dbus_finalize   (GObject *object);
+
+static void _messages_service_server_watch  (void);
+static void _messages_service_server_attention_requested (void);
+static void _messages_service_server_icon_shown (void);
+
+#include "messages-service-server.h"
+
+G_DEFINE_TYPE (MessageServiceDbus, message_service_dbus, G_TYPE_OBJECT);
+
+static void
+message_service_dbus_class_init (MessageServiceDbusClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	g_type_class_add_private (klass, sizeof (MessageServiceDbusPrivate));
+
+	object_class->dispose = message_service_dbus_dispose;
+	object_class->finalize = message_service_dbus_finalize;
+
+	return;
+}
+
+static void
+message_service_dbus_init (MessageServiceDbus *self)
+{
+	return;
+}
+
+static void
+message_service_dbus_dispose (GObject *object)
+{
+
+
+	G_OBJECT_CLASS (message_service_dbus_parent_class)->dispose (object);
+	return;
+}
+
+static void
+message_service_dbus_finalize (GObject *object)
+{
+
+
+	G_OBJECT_CLASS (message_service_dbus_parent_class)->finalize (object);
+	return;
+}
+
+static void
+_messages_service_server_watch  (void)
+{
+
+}
+
+static void
+_messages_service_server_attention_requested (void)
+{
+
+}
+
+static void
+_messages_service_server_icon_shown (void)
+{
+
+}
+
---
+++ src/messages-service-dbus.h
@@ -0,0 +1,37 @@
+#ifndef __MESSAGE_SERVICE_DBUS_H__
+#define __MESSAGE_SERVICE_DBUS_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define MESSAGE_SERVICE_DBUS_TYPE            (message_service_dbus_get_type ())
+#define MESSAGE_SERVICE_DBUS(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), MESSAGE_SERVICE_DBUS_TYPE, MessageServiceDbus))
+#define MESSAGE_SERVICE_DBUS_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), MESSAGE_SERVICE_DBUS_TYPE, MessageServiceDbusClass))
+#define IS_MESSAGE_SERVICE_DBUS(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MESSAGE_SERVICE_DBUS_TYPE))
+#define IS_MESSAGE_SERVICE_DBUS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), MESSAGE_SERVICE_DBUS_TYPE))
+#define MESSAGE_SERVICE_DBUS_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), MESSAGE_SERVICE_DBUS_TYPE, MessageServiceDbusClass))
+
+#define MESSAGE_SERVICE_DBUS_SIGNAL_ATTENTION_CHANGED  "attention-changed"
+#define MESSAGE_SERVICE_DBUS_SIGNAL_ICON_CHANGED       "icon-changed"
+
+typedef struct _MessageServiceDbus      MessageServiceDbus;
+typedef struct _MessageServiceDbusClass MessageServiceDbusClass;
+
+struct _MessageServiceDbusClass {
+	GObjectClass parent_class;
+
+	void (*attention_changed) (gboolean dot);
+	void (*icon_changed) (gboolean hidden);
+};
+
+struct _MessageServiceDbus {
+	GObject parent;
+};
+
+GType message_service_dbus_get_type (void);
+
+G_END_DECLS
+
+#endif
