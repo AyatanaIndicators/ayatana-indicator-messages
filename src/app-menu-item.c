e7abefb09efca68e2101def3ff445d546fe74bac
Ted Gould <ted@canonical.com>
2009-09-05 11:43:36 -0500

    No atoi, no stdlib.h, that's how it is.

--- src/app-menu-item.c
+++ src/app-menu-item.c
@@ -24,7 +24,6 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config.h"
 #endif
 
-#include <stdlib.h>
 #include <glib/gi18n.h>
 #include <gio/gdesktopappinfo.h>
 #include "app-menu-item.h"
