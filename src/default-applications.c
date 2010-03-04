4665a089117f7fc0eb537aedcca0d7d10168b12d
Ted Gould <ted@gould.cx>
2010-03-04 00:20:22 -0600

    Actually searching through the list

--- src/default-applications.c
+++ src/default-applications.c
@@ -16,8 +16,22 @@ struct default_db_t default_db[] = {
 const gchar *
 get_default_name (gchar * desktop_path)
 {
+	g_return_val_if_fail(desktop_path != NULL, NULL);
+	gchar * basename = g_path_get_basename(desktop_path);
+	g_return_val_if_fail(basename != NULL, NULL);
+
+	gint i;
+	for (i = 0; default_db[i].desktop_file != NULL; i++) {
+		if (g_strcmp0(default_db[i].desktop_file, basename) == 0) {
+			break;
+		}
+	}
+
+	g_free(basename);
+
+	if (default_db[i].name != NULL) {
+		return default_db[i].name;
+	}
 
 	return NULL;
-
-
 }
