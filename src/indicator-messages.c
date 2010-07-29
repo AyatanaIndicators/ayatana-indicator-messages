32e8f9ff37ace0f23895b762d5c5a4a5696a093e
David Barth <david.barth@canonical.com>
2010-07-29 16:03:42 +0200

    fix typos to build and clean up comments

--- src/indicator-messages.c
+++ src/indicator-messages.c
@@ -298,7 +298,7 @@ application_triangle_draw_cb (GtkWidget *widget, GdkEventExpose *event, gpointer
 	cairo_t *cr;
 	int x, y, arrow_width, arrow_height;
 
-	if (!GTK_IS_WIDGET (widget)) return;
+	if (!GTK_IS_WIDGET (widget)) return FALSE;
 
 	/* get style */
 	style = gtk_widget_get_style (widget);
@@ -355,7 +355,7 @@ numbers_draw_cb (GtkWidget *widget, GdkEventExpose *event, gpointer data)
 	PangoLayout * layout;
 	gint font_size = RIGHT_LABEL_FONT_SIZE;
 
-	if (!GTK_IS_WIDGET (widget)) return;
+	if (!GTK_IS_WIDGET (widget)) return FALSE;
 
 	/* get style */
 	style = gtk_widget_get_style (widget);
@@ -570,11 +570,7 @@ new_indicator_item (DbusmenuMenuitem * newitem, DbusmenuMenuitem * parent, Dbusm
 	   item. */
 	mi_data->right = gtk_label_new(dbusmenu_menuitem_property_get(newitem, INDICATOR_MENUITEM_PROP_RIGHT));
 	gtk_size_group_add_widget(indicator_right_group, mi_data->right);
-
-	/* Doesn't work, look numbers_draw_cb. */
-	/* PangoLayout * right_layout = gtk_label_get_layout (GTK_LABEL(mi_data->right));
-	   font_size = pango_font_description_get_size (pango_layout_get_font_description (right_layout)); */
-
+	/* install extra decoration overlay */
 	g_signal_connect (G_OBJECT (mi_data->right), "expose_event",
 	                  G_CALLBACK (numbers_draw_cb), NULL);
 
