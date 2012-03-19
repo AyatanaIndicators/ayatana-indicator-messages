ab5696a78727fc9d20a5679481876e926baf7de7
Allan LeSage <allanlesage@gmail.com>
2012-03-19 13:38:41 -0500

    Corrected a Makefile merge omission for tests, stripped down dummy test.

--- test/test-gtest.cpp
+++ test/test-gtest.cpp
@@ -8,9 +8,5 @@ extern "C" {
 
 TEST(LauncherMenuItem, NameInitialized) {
   g_type_init();
-  //const gchar * expected = "foo";
-  //LauncherMenuItem * test_li = launcher_menu_item_new ("foo");
-  //gchar * result = launcher_menu_item_get_name(test_li);
-  //EXPECT_EQ(0, strcmp("foo", result));
   EXPECT_TRUE(true);
 }
