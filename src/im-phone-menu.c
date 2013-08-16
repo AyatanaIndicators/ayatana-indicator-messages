08739c5fe918d9aef5b3f2f8b0e86a2fbed08a7f
Ted Gould <ted@gould.cx>
2013-08-16 14:01:36 -0500

    Dropping indicator prefixes as they're set up with the base namespace of the section

--- src/im-phone-menu.c
+++ src/im-phone-menu.c
@@ -72,7 +72,7 @@ im_phone_menu_constructed (GObject *object)
 
     clear_section = g_menu_new ();
 
-    item = g_menu_item_new ("Clear All", "indicator.remove-all");
+    item = g_menu_item_new ("Clear All", "remove-all");
     g_menu_item_set_attribute (item, "x-canonical-type", "s", "com.canonical.indicator.button");
     g_menu_append_item (clear_section, item);
 
@@ -167,7 +167,7 @@ im_phone_menu_add_message (ImPhoneMenu     *menu,
   g_return_if_fail (IM_IS_PHONE_MENU (menu));
   g_return_if_fail (app_id);
 
-  action_name = g_strconcat ("indicator.", app_id, ".msg.", id, NULL);
+  action_name = g_strconcat (app_id, ".msg.", id, NULL);
 
   item = g_menu_item_new (title, action_name);
 
@@ -229,7 +229,7 @@ im_phone_menu_add_source (ImPhoneMenu     *menu,
   g_return_if_fail (IM_IS_PHONE_MENU (menu));
   g_return_if_fail (app_id != NULL);
 
-  action_name = g_strconcat ("indicator.", app_id, ".src.", id, NULL);
+  action_name = g_strconcat (app_id, ".src.", id, NULL);
 
   item = g_menu_item_new (label, action_name);
   g_menu_item_set_attribute (item, "x-canonical-type", "s", "com.canonical.indicator.messages.sourceitem");
